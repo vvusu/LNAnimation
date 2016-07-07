f1e806d52019d379d6022504c02f2d68e2598172
vvusu <vvusu@Lin-Mac-mini.local>
2016-07-07 15:29:58 +0800

    add code

---
+++ LNAnimation/AnimationVC/AnimatedPageControl/LNAnimatedPageControl/LNAnimatedPageControl.swift
@@ -0,0 +1,35 @@
+//
+//  LNAnimatedPageControl.swift
+//  LNAnimation
+//
+//  Created by vvusu on 7/6/16.
+//  Copyright © 2016 vvusu. All rights reserved.
+//
+
+import UIKit
+
+enum IndicatorStyle {
+    case Circle
+    case Rect
+}
+
+class LNAnimatedPageControl: UIView {
+    
+    var pageCount = 0
+    var selectedPage = 0
+    var selectedColor = UIColor.red()
+    var unSelectedColor = UIColor.white()
+    var shouldShowProgressLine = true
+    var bindScrollView = UIScrollView.init();
+    var swipeEnable = true;
+    var indicatorSize = 0;
+    
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ LNAnimation/AnimationVC/AnimatedPageControl/LNAnimatedPageControl/LNRotateRect.swift
@@ -0,0 +1,13 @@
+//
+//  LNRotateRect.swift
+//  LNAnimation
+//
+//  Created by vvusu on 7/6/16.
+//  Copyright © 2016 vvusu. All rights reserved.
+//
+
+import UIKit
+
+class LNRotateRect: LNIndicator {
+
+}
